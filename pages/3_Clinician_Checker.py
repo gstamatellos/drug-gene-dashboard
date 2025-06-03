abdcda219c04a92f82d9aec8d1218a86a1e035de
Giorgos Stamatellos <stamatellosgeo@gmail.com>
2025-06-03 20:13:43 +0300

    Update 3_Clinician_Checker.py

--- pages/3_Clinician_Checker.py
+++ pages/3_Clinician_Checker.py
@@ -5,8 +5,7 @@ st.markdown("""
 This tool helps clinicians assess **pharmacogenomic risks before prescribing a drug**, using clinical annotations from **PharmGKB**.
 
 - Identifies **gene-drug interactions** that affect **safety, dosing, or efficacy**  
-- Highlights high-risk associations (e.g., **Level 1A/1B**, **toxicity**, **non-response**)  
-- Filters results by **phenotype category** or **evidence level**
-- Suggests a gene panel — a list of genes worth testing — to support personalized treatment decisions.
+- Highlights high-risk associations (e.g., **Level 1A/1B**, **toxicity**)
+- Suggests a **gene panel** — a list of genes worth testing — to support personalized treatment decisions
 
 """)

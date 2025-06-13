874c63be5d29d26273ebb936a199c8a5799719d6
Giorgos Stamatellos <stamatellosgeo@gmail.com>
2025-06-13 20:19:57 +0300

    Update 3_Clinician_Checker.py

--- pages/3_Clinician_Checker.py
+++ pages/3_Clinician_Checker.py
@@ -135,4 +135,5 @@ if search_input:
                 """)
         else:
             st.warning(f"No variant annotations found for '{search_input}'. Try another term.")
+            st.download_button("📥 Download empty CSV", data=pd.DataFrame().to_csv(index=False), file_name=f"{search_input}_{search_type.lower()}_variant_safety.csv")
 

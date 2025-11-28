7f61205a1274484c6261f0be8a8446f026189e7b
Giorgos Stamatellos <stamatellosgeo@gmail.com>
2025-11-28 21:20:38 +0200

    Update Search.py

--- Search.py
+++ Search.py
@@ -10,16 +10,7 @@ st.markdown("""
 This app allows you to search **drug–gene interactions** from the [DGIdb](https://dgidb.org) API 
 and **pharmacogenomic variant-drug annotations** data from [ClinPGx](https://www.clinpgx.org/).
 
-After searching, use the sidebar to explore:
-- **Results Tables** 
-- **Interaction - Annotation Visuals**  
+Use the **sidebar** to explore:
+- **Drug - Gene Interaction Tables and Visuals** 
+- **Clinician Safety Checker**  
 """)
-
-st.markdown("""
-""")
-
-# --- Clinician Safety Checker button in main page ---
-st.markdown("**NEW: Clinician Safety Checker**")
-if st.button("Visit page"):
-    st.session_state["goto_clinician_checker"] = True
-    st.info("Please click the sidebar page **Clinician Safety Checker** to continue!")
